efac67f4d773e69967fdddbf5d337eff53269c21
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-09 18:40:41 -0400

    Update main.py
    - added ability for the user to train multiple models and choose which one to use for classification 
    - added support to handle other types of files as well. For example
    - added ability for user to specify their own labels instead of using the names of the subdirectories in the training directory. 
    - program now creates subdirectories based on some criterion (date)

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 import shutil
 import joblib
 import nltk
+import magic
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, simpledialog
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from filetype import guess
+from datetime import datetime
 
 nltk.download('stopwords')
 
@@ -20,131 +21,121 @@ vectorizer = CountVectorizer(stop_words=stopwords)
 # Create the GUI
 root = tk.Tk()
 
-# Create the model variable
+# Create the model variable and models list
 model = tk.StringVar()
+models = []
 
 # Create backup directory path
 backup_dir = os.path.expanduser("~/Desktop_backup")
 
+# Function to make a backup
 def make_backup():
-    # Path to the desktop
     desktop = os.path.expanduser("~/Desktop")
-
-    # Make a backup of the entire desktop
     if not os.path.exists(backup_dir):
         shutil.copytree(desktop, backup_dir)
     else:
         messagebox.showinfo("Error", "Backup already exists!")
 
+# Function to restore from a backup
 def restore_backup():
-    # Path to the desktop
     desktop = os.path.expanduser("~/Desktop")
-
-    # Restore the backup
     if os.path.exists(backup_dir):
         shutil.rmtree(desktop)
         shutil.copytree(backup_dir, desktop)
     else:
         messagebox.showinfo("Error", "No backup found!")
 
+# Function to train a model
 def train_model():
-    # Ask the user to select a directory
     dir_path = filedialog.askdirectory()
     
     if not dir_path:
         return
 
-    # Get the list of all subdirectories
     subdirs = [d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))]
 
     if not subdirs:
         messagebox.showinfo("Error", "No subdirectories found!")
         return
     
-    # Create lists to store the file contents and their labels
     texts = []
     labels = []
     
-    # Load the files
     for subdir in subdirs:
         files = os.listdir(os.path.join(dir_path, subdir))
         for file in files:
             with open(os.path.join(dir_path, subdir, file), 'r') as f:
                 texts.append(f.read())
-                labels.append(subdir)
+                labels.append(get_label(os.path.join(dir_path, subdir)))
     
-    # Convert the texts into feature vectors
     X = vectorizer.fit_transform(texts)
-    
-    # Train the model
-    model.set(MultinomialNB().fit(X, labels))
-    
-    # Save the model to a file
-    joblib.dump(model.get(), 'model.joblib')
+    new_model = MultinomialNB().fit(X, labels)
+    models.append(new_model)
+    model_list.insert(tk.END, f"Model {len(models)}")
 
+# Function to ask the user for a label for each subdirectory
+def get_label(dir_path):
+    label = simpledialog.askstring("Input", f"Enter label for {dir_path}")
+    return label
+
+# Function to classify and move files
 def classify_files():
-    # Check if a model is loaded
     if model.get() == '':
         messagebox.showinfo("Error", "No model loaded!")
         return
-    
-    # Path to the desktop
-    path = os.path.expanduser("~/Desktop")
 
-    # Get the list of all files in the directory
+    path = os.path.expanduser("~/Desktop")
     files = os.listdir(path)
+    mime = magic.Magic(mime=True)
     
     for file in files:
         filepath = os.path.join(path, file)
-
-        # Skip directories
         if os.path.isdir(filepath):
             continue
         
-        # Check if the file is a text file
-        kind = guess(filepath)
-        
-        if kind is None:
-            print(f"Cannot guess file type for {file}!")
-            continue
-
-        if "text" in kind.mime:
+        mimetype = mime.from_file(filepath)
+        if "text" in mimetype:
             with open(filepath, 'r') as f:
                 text = f.read()
                 features = vectorizer.transform([text])
                 label = model.get().predict(features)
                 
-                # Create the directory if it doesn't already exist
-                new_dir = os.path.join(path, label[0])
+                date = datetime.fromtimestamp(os.path.getmtime(filepath)).strftime('%Y-%m-%d')
+                new_dir = os.path.join(path, label[0], date)
                 if not os.path.exists(new_dir):
-                    os.mkdir(new_dir)
+                    os.makedirs(new_dir)
                 
-                # Move the file
                 shutil.move(filepath, new_dir)
 
+# Function to load a model
 def load_model():
-    # Ask the user to select a file
     file_path = filedialog.askopenfilename()
-    
     if not file_path:
         return
-
-    # Load the model from the file
     try:
         model.set(joblib.load(file_path))
     except:
         messagebox.showinfo("Error", "Failed to load the model!")
 
-# Create the buttons
+# Function to select a model
+def select_model():
+    selected_index = model_list.curselection()[0]
+    model.set(models[selected_index])
+
+# Create the buttons and listbox
 train_button = tk.Button(root, text="Train a New Model", command=train_model)
 classify_button = tk.Button(root, text="Organize Desktop", command=classify_files)
 load_button = tk.Button(root, text="Load a Model", command=load_model)
+select_button = tk.Button(root, text="Select a Model", command=select_model)
 backup_button = tk.Button(root, text="Backup Desktop", command=make_backup)
 restore_button = tk.Button(root, text="Restore Desktop", command=restore_backup)
+model_list = tk.Listbox(root)
 
 train_button.pack()
 classify_button.pack()
 load_button.pack()
+select_button.pack()
+model_list.pack()
 backup_button.pack()
 restore_button.pack()
 

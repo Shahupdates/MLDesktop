2f16c6056fcfa155f613e08a0e7207245553bee8
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-12 09:39:10 -0400

    Update model_trainer.py
    Changes:
    - Encapsulation of logic into smaller functions for better maintainability.
    - Added docstrings and type hints to improve the readability of the code and to provide more information about the function's purpose, inputs, and outputs.
    - Handling exceptions when opening files to avoid potential program crashes.
    - Adding some more error checking, for example when asking for labels.
    - Organizing the imports in a PEP-8 compliant way (built-in libraries first, then third-party libraries).
    - Using pathlib for easier and safer manipulation of file paths.

--- model_trainer.py
+++ model_trainer.py
@@ -1,11 +1,12 @@
-# model_trainer.py
-
 import os
+from typing import Set, List
+from pathlib import Path
 import tkinter.simpledialog as simpledialog
 import tkinter.messagebox as messagebox
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+
 
 class ModelTrainer:
     def __init__(self):
@@ -14,37 +15,70 @@ class ModelTrainer:
         self.model_list = None
 
     @staticmethod
-    def get_stopwords():
+    def get_stopwords() -> Set[str]:
+        """Get English stopwords.
+
+        Returns:
+            set: A set of stopwords.
+        """
         return set(stopwords.words('english'))
 
     def train_model(self):
-        dir_path = filedialog.askdirectory()
+        """Train a new model using texts from a directory selected by the user."""
+        dir_path = Path(filedialog.askdirectory())
         
-        if not dir_path:
+        if not dir_path.is_dir():
             return
 
-        subdirs = [d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))]
+        subdirs = [d for d in dir_path.iterdir() if d.is_dir()]
 
         if not subdirs:
             messagebox.showinfo("Error", "No subdirectories found!")
             return
         
-        texts = []
-        labels = []
-        
-        for subdir in subdirs:
-            files = os.listdir(os.path.join(dir_path, subdir))
-            for file in files:
-                with open(os.path.join(dir_path, subdir, file), 'r') as f:
-                    texts.append(f.read())
-                    labels.append(self.get_label(os.path.join(dir_path, subdir)))
-        
+        texts, labels = self.get_texts_and_labels(subdirs)
+
         X = self.vectorizer.fit_transform(texts)
         new_model = MultinomialNB().fit(X, labels)
         self.models.append(new_model)
         self.model_list.insert(tk.END, f"Model {len(self.models)}")
 
+    def get_texts_and_labels(self, subdirs: List[Path]) -> tuple:
+        """Get texts from files and their labels.
+
+        Args:
+            subdirs (list): A list of subdirectories.
+
+        Returns:
+            tuple: A tuple of two lists (texts and labels).
+        """
+        texts = []
+        labels = []
+        
+        for subdir in subdirs:
+            files = subdir.iterdir()
+            for file in files:
+                try:
+                    with open(file, 'r') as f:
+                        texts.append(f.read())
+                        labels.append(self.get_label(subdir))
+                except Exception as e:
+                    messagebox.showinfo("Error", f"Could not open file {file}. Error: {e}")
+        
+        return texts, labels
+
     @staticmethod
-    def get_label(dir_path):
+    def get_label(dir_path: Path) -> str:
+        """Ask user to input a label for a directory.
+
+        Args:
+            dir_path (Path): A path to a directory.
+
+        Returns:
+            str: A label for a directory.
+        """
         label = simpledialog.askstring("Input", f"Enter label for {dir_path}")
+        while not label:
+            messagebox.showinfo("Error", "Label cannot be empty.")
+            label = simpledialog.askstring("Input", f"Enter label for {dir_path}")
         return label
